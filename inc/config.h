e13b0dfb2a8a596bedd371f90bf812b12279e443
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-02-07 21:42:06 +0000

    added config usage to main file

--- inc/config.h
+++ inc/config.h
@@ -1,6 +1,8 @@
 #pragma once
 
 namespace config {
+    constexpr unsigned int WINDOW_WIDTH = 1920;
+    constexpr unsigned int WINDOW_HEIGHT = 1080;
     constexpr unsigned int FRAME_RATE = 60;
     constexpr float ZOOM_FACTOR = 1.1f;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "application.h"
+#include "config.h"
 
 int main() {
-    Application app({1920, 1080}, "Pathfinding Visualiser");
+    Application app({ config::WINDOW_WIDTH, config::WINDOW_HEIGHT }, "Pathfinding Visualiser");
     app.run();
     return 0;
 }
